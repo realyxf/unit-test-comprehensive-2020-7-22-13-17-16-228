2cf36d610bed173928422a12911044c8f4dc16ce
realyxf <997915421@qq.com>
2020-07-23 21:31:20 +0800

    [GuessNumber][Fennie] feature: pass should_return_4A0B_when_guess_given_answer_1234_and_inputguess_1234

--- src/main/java/example/AnswerGenerator.java
+++ src/main/java/example/AnswerGenerator.java
@@ -1,5 +1,5 @@
 package example;
 
 public interface AnswerGenerator {
-    Object generate();
+    int[] generate();
 }
--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,11 +1,19 @@
 package example;
 
 public class GuessNumber {
+    private int[] answerGenerator;
+    int countA=0;
+    int countB=0;
     public GuessNumber(AnswerGenerator answerGenerator) {
-
+        this.answerGenerator = answerGenerator.generate();
     }
 
     public String guess(int[] inputGuess) {
-        return null;
+        for(int index = 0; index < inputGuess.length; index++){
+            if(inputGuess[index] == answerGenerator[index]){
+                countA++;
+            }
+        }
+        return String.format("%sA%sB",countA,countB);
     }
 }
