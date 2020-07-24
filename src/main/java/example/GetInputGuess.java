e6193793bcfbd93b021098cff8e4c8d6b20d60b2
realyxf <997915421@qq.com>
2020-07-24 13:01:50 +0800

    [GuessNumber][Fennie] feature:method getInput of GetInputGuess

--- src/main/java/example/GetInputGuess.java
+++ src/main/java/example/GetInputGuess.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class GetInputGuess {
     public int[] getInput() {
-        return null;
+        System.out.println("please input four number from 0 to 9");
+        Scanner scanner = new Scanner(System.in);
+        int[] inputGuess=new int[4];
+        for(int i=0;i<inputGuess.length;i++){
+            inputGuess[i]=scanner.nextInt();
+        }
+        return inputGuess;
     }
 }
