79152c08a5cee89be069a227d3a92299ce7650bf
realyxf <997915421@qq.com>
2020-07-24 13:03:25 +0800

    [GuessNumber][Fennie] bug-fix:set the num do not repeat

--- src/main/java/example/AnswerGeneratorClass.java
+++ src/main/java/example/AnswerGeneratorClass.java
@@ -1,16 +1,28 @@
 package example;
 
+import java.util.HashSet;
 import java.util.Random;
 
 public class AnswerGeneratorClass implements AnswerGenerator{
     @Override
     public int[] generate() {
-        int[] answerGenerator = new int[4];
+        HashSet<Integer> answerSet = new HashSet<>();
         Random random = new Random();
-        for (int answerIndex = 0; answerIndex < 4; answerIndex++) {
-            int answerItem = random.nextInt(9); // 生成[0,9]区间的整数
-            answerGenerator[answerIndex] = answerItem;
+        int[] answerGenerator = new int[4];
+        do {
+            int answerSetItem = random.nextInt(9);
+            if(!answerSet.contains(answerSetItem)){
+                answerSet.add(answerSetItem);
+            }
+        } while (answerSet.size()<4);
+
+        int answerGeneratorIndex = 0;
+
+        for (int answerSetItem : answerSet) {
+            answerGenerator[answerGeneratorIndex] = answerSetItem;
+            answerGeneratorIndex++;
         }
+
         return answerGenerator;
     }
 }
--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -2,16 +2,21 @@ package example;
 
 public class Main {
     public static void main(String[] args) {
-
+//         test generate answer
         AnswerGeneratorClass a = new AnswerGeneratorClass();
-        int[] arr = a.generate();
+        int[] arr = a.generate(); //TODO arr
         for(int i=0;i<4;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
         }
-        GuessNumber g = new GuessNumber(a);
-        int[] input = {1,2,3,4};
-        String str = g.guess(input);
-        System.out.println(str);
+        GetInputGuess inputGuess = new GetInputGuess();
+        int[] input= inputGuess.getInput();
+        for(int inputIndex=0;inputIndex<4;inputIndex++){
+            System.out.print(input[inputIndex]);
+        }
+
+        GuessNumber guessNumber = new GuessNumber(a);
+        String result = guessNumber.guess(input);
+        System.out.println(result);
 
     }
 }
