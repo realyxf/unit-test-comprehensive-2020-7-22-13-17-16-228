2da39fb97e31d73fe17f7b7c208fb5c21ad3ebda
realyxf <997915421@qq.com>
2020-07-27 21:23:50 +0800

    [GuessNumber][Fennie] test: should_return_wrong_input_input_again_when_getInput_given_1_1_2_3

--- src/test/java/example/GetInputGuessTest.java
+++ src/test/java/example/GetInputGuessTest.java
@@ -1,14 +1,24 @@
 package example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 public class GetInputGuessTest {
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
     @Test
     void should_return_1234_when_checkout_given_inputguess_1234() {
         //given
@@ -21,6 +31,25 @@ public class GetInputGuessTest {
 
         //then
         assertEquals("[1, 2, 3, 4]",result);
+
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
+    @Test
+    void should_return_wrong_input_input_again_when_getInput_given_1_1_2_3() {
+        //given
+        int[] inputguess = {1,1,2,3};
+        GetInputGuess getInputGuess = Mockito.mock(GetInputGuess.class);
+        when(getInputGuess.getInput()).thenReturn(inputguess);
+
+        //when
+        getInputGuess.isValidInput(inputguess);
+
+        //then
+
 
     }
 }
