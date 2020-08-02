c6d416c521c26ea5e0451c6eb587f1b82892b7e5
realyxf <997915421@qq.com>
2020-08-02 22:28:37 +0800

    [GuessNumber] [Fennie] test: add and pass should_return_4_different_number_when_generate_answer()

--- src/test/java/example/AnswerGeneratorClassTest.java
+++ src/test/java/example/AnswerGeneratorClassTest.java
@@ -1,6 +1,7 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+import java.util.HashSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnswerGeneratorClassTest {
@@ -13,13 +14,33 @@ public class AnswerGeneratorClassTest {
     void should_return_the_length_is_4__when_generate_answer() {
         //given
         answerGeneratorClass = new AnswerGeneratorClass();
-        int[] answer = answerGeneratorClass.generate();
 
         //when
+        int[] answer = answerGeneratorClass.generate();
         int result = answer.length;
 
         //then
         assertEquals(4, result);
+    }
+
+    @Test
+    void should_return_4_different_number_when_generate_answer() {
+        //given
+        answerGeneratorClass = new AnswerGeneratorClass();
+
+        //when
+        boolean result = true;
+        int[] answer = answerGeneratorClass.generate();
+        HashSet<Integer> answerSet = new HashSet<>();
+        for (int answerItem : answer) {
+            if (answerSet.contains(answerItem)) {
+                result = false;
+            }
+            answerSet.add(answerItem);
+        }
+
+        //then
+        assertEquals(true, result);
     }
 
 }
